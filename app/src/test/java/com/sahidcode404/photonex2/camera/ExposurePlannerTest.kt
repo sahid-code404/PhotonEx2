9072ea0de6e08237166a24ebf242b484e53c1b16
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-28 17:35:23 +0530

    test: cover dynamic and manual burst planning

---
+++ app/src/test/java/com/sahidcode404/photonex2/camera/ExposurePlannerTest.kt
@@ -0,0 +1,27 @@
+package com.sahidcode404.photonex2.camera
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExposurePlannerTest {
+    @Test
+    fun daylightUsesShortBurst() {
+        assertEquals(3, ExposurePlanner.chooseFrameCount(null, 1_000_000L, 100))
+    }
+
+    @Test
+    fun darkerScenesIncreaseBurstLength() {
+        val day = ExposurePlanner.chooseFrameCount(null, 2_000_000L, 100)
+        val indoor = ExposurePlanner.chooseFrameCount(null, 10_000_000L, 400)
+        val night = ExposurePlanner.chooseFrameCount(null, 30_000_000L, 1600)
+        assert(day < indoor)
+        assert(indoor < night)
+        assertEquals(12, night)
+    }
+
+    @Test
+    fun perLensOverrideWins() {
+        assertEquals(7, ExposurePlanner.chooseFrameCount(7, 50_000_000L, 3200))
+        assertEquals(12, ExposurePlanner.chooseFrameCount(99, 1_000_000L, 100))
+    }
+}
